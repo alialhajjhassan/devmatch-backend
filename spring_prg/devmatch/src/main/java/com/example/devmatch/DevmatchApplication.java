bda55e340adee9abf5ad0216c27d15373423c412
alihjhsn <ali.hajhassan@edu.unito.it>
2026-05-03 23:56:51 +0200

    Initial project setup with Spring Boot

---
+++ spring_prg/devmatch/src/main/java/com/example/devmatch/DevmatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.devmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevmatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevmatchApplication.class, args);
+	}
+
+}
---
+++ spring_prg/devmatch/src/test/java/com/example/devmatch/DevmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
